36da6e881f693d47315c3c3c1521e8436dcde0ec
Mohit Saran <48322980+kinghacker0@users.noreply.github.com>
2020-04-27 16:07:33 +0530

    Update Termux-install.sh

--- Termux-install.sh
+++ Termux-install.sh
@@ -1,5 +1,5 @@
 apt install apksigner
 git clone https://github.com/Lexiie/Termux-Apktool
 cd Termux-Apktool && dpkg -i apktool_2.3.4_all.deb
-cd .. && rm Termux-Apktool
+cd .. && rm -rf Termux-Apktool
 echo -e "Installation Complete";
