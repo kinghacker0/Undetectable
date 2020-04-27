b8b5075ed47d8e597ca7ed3b6e065f8a7388748b
Mohit Saran <48322980+kinghacker0@users.noreply.github.com>
2020-04-27 15:57:54 +0530

    Add files via upload

---
+++ Termux-install.sh
@@ -0,0 +1,6 @@
+apt install apksigner
+apt install wget
+wget https://github.com/Lexiie/Termux-Apktool/blob/master/apktool_2.3.4_all.deb
+dpkg -i apktool_2.3.4_all.deb
+rm apktool_2.3.4_all.deb
+echo -e "Installation Complete";
